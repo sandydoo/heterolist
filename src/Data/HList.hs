7411b5c3e2d8d7eb25d010cd5d573957de2ab94a
sandydoo <hey@sandydoo.me>
2022-07-25 13:44:03 +0400

    Format and add tests for Show

--- src/Data/HList.hs
+++ src/Data/HList.hs
@@ -1,25 +1,27 @@
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Data.HList
-  ( HList(..)
-  , hlength
-  , happend
-  ) where
+  ( HList (..),
+    hlength,
+    happend,
+  )
+where
 
 import Data.Kind
 
 type HList :: [Type] -> Type
 data HList xs where
   HNil :: HList '[]
-  HCons :: x -> HList xs -> HList (x ': xs )
+  HCons :: x -> HList xs -> HList (x ': xs)
 
 infixr 5 `HCons`
 
@@ -27,29 +29,24 @@ instance Show (HList '[]) where
   show _ = "[]"
 
 instance (Show x, Show (HList xs)) => Show (HList (x ': xs)) where
-  show (x `HCons` xs) = 
-    let
-      '[' : s = show xs
-    in
-    "[" ++ show x ++ (if s == "]" then s else ", " ++ s)  
-
+  show (x `HCons` xs) =
+    let '[' : s = show xs
+     in "[" ++ show x ++ (if s == "]" then s else ", " ++ s)
 
 instance Eq (HList '[]) where
   _ == _ = True
 
 instance (Eq x, Eq (HList xs)) => Eq (HList (x ': xs)) where
   (x `HCons` xs) == (y `HCons` ys) = x == y && xs == ys
-  
 
 hlength :: HList xs -> Int
 hlength HNil = 0
 hlength (_ `HCons` xs) = 1 + hlength xs
 
-
 type Append :: forall a. [a] -> [a] -> [a]
 type family Append xs ys where
-  Append '[]       ys = ys
-  Append (x : xs)  ys = x ': Append xs ys
+  Append '[] ys = ys
+  Append (x : xs) ys = x ': Append xs ys
 
 happend :: HList xs -> HList ys -> HList (Append xs ys)
 happend HNil ys = ys
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,27 +8,29 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "length" $ do
-        it "length" $
-            hlength (42 `HCons` 'h' `HCons` HNil)
-            `shouldBe` 2
-    
-    describe "append" $ do
-        it "appends empty lists" $
-            happend HNil HNil `shouldBe` HNil
-                        
-        it "appends list to empty list " $
-            let
-                xs = HNil
-                ys = 1 `HCons` 'b' `HCons` HNil
-            in
-            xs `happend` ys `shouldBe` ys
+  describe "show" $ do
+    it "shows an empty list" $
+      show HNil `shouldBe` "[]"
 
-        it "appends lists" $
-            let
-                xs = 'a' `HCons` True `HCons` HNil
-                ys = 1 `HCons` 'b' `HCons` HNil
-                zs = 'a' `HCons` True `HCons` 1 `HCons` 'b' `HCons` HNil
-            in
-            xs `happend` ys `shouldBe` zs
-            
+    it "shows a list" $
+      show (1 `HCons` 'a' `HCons` HNil) `shouldBe` "[1, 'a']"
+
+  describe "length" $ do
+    it "length" $
+      hlength (42 `HCons` 'h' `HCons` HNil)
+        `shouldBe` 2
+
+  describe "append" $ do
+    it "appends empty lists" $
+      happend HNil HNil `shouldBe` HNil
+
+    it "appends list to empty list " $
+      let xs = HNil
+          ys = 1 `HCons` 'b' `HCons` HNil
+       in xs `happend` ys `shouldBe` ys
+
+    it "appends lists" $
+      let xs = 'a' `HCons` True `HCons` HNil
+          ys = 1 `HCons` 'b' `HCons` HNil
+          zs = 'a' `HCons` True `HCons` 1 `HCons` 'b' `HCons` HNil
+       in xs `happend` ys `shouldBe` zs
